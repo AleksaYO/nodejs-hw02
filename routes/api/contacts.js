c16823f9bffdad5030255188101ca80d9f138d37
AleksaYO <yura.aleksa.98@gmail.com>
2023-07-16 12:14:47 +0300

    fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -21,7 +21,11 @@ router.get("/", async (req, res, next) => {
 router.get("/:contactId", async (req, res, next) => {
   try {
     const contact = await getContactById(req.params.contactId);
-    res.json(contact);
+    if (contact) {
+      res.json(contact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
   } catch (error) {
     next(error);
   }
@@ -29,8 +33,13 @@ router.get("/:contactId", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const newContact = await addContact(req.body);
-    res.json(newContact);
+    const { name, email, phone } = req.body;
+    if (name && email && phone) {
+      const newContact = await addContact(req.body);
+      res.status(201).json(newContact);
+    } else {
+      res.status(400).json({ message: "Missing required field" });
+    }
   } catch (error) {
     next(error);
   }
@@ -40,6 +49,7 @@ router.delete("/:contactId", async (req, res, next) => {
   try {
     const delContact = await removeContact(req.params.contactId);
     if (delContact) res.json({ message: "Contact delete" });
+    else res.status(404).json({ message: "Not Found" });
   } catch (error) {
     next(error);
   }
@@ -47,8 +57,13 @@ router.delete("/:contactId", async (req, res, next) => {
 
 router.put("/:contactId", async (req, res, next) => {
   try {
-    const contacts = await updateContacts(req.params.contactId, req.body);
-    res.json(contacts);
+    const { name, email, phone } = req.body;
+    if (name && email && phone) {
+      const contacts = await updateContacts(req.params.contactId, req.body);
+      res.json(contacts);
+    } else {
+      res.status(400).json({ message: "Missing fields" });
+    }
   } catch (error) {
     next(error);
   }
